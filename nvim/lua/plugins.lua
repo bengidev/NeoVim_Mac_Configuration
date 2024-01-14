da628d1beb18c46ee0333ae0aff71bd4c48fdcaa
B e n g i <bambang.trd17@gmail.com>
2024-01-15 05:33:33 +0700

    feat: use gitsigns to ease manage git functions

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -91,7 +91,7 @@ return packer.startup(function(use)
 	use({ "nvim-treesitter/nvim-treesitter" })
 
 	-- Git
-	--use { "lewis6991/gitsigns.nvim" }
+	use({ "lewis6991/gitsigns.nvim" })
 
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
