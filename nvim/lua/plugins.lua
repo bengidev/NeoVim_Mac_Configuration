362159bcf366d006bb273208aa2563231d61be2d
B e n g i <bambang.trd17@gmail.com>
2024-01-15 05:50:14 +0700

    feat: use nvim-tree to enable workspace structures

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -48,8 +48,8 @@ return packer.startup(function(use)
 	use({ "windwp/nvim-autopairs" }) -- Autopairs, integrates with both cmp and treesitter
 	use({ "numToStr/Comment.nvim" })
 	use({ "JoosepAlviste/nvim-ts-context-commentstring" })
-	--use { "kyazdani42/nvim-web-devicons" }
-	--use { "kyazdani42/nvim-tree.lua" }
+	use({ "kyazdani42/nvim-web-devicons" })
+	use({ "kyazdani42/nvim-tree.lua" })
 	--use { "akinsho/bufferline.nvim" }
 	--use { "moll/vim-bbye" }
 	--use { "nvim-lualine/lualine.nvim" }
